c7ad4f90c986986c9214e097e04e603d01c624d1
Sohil Veljee <sohilveljee@Sohils-MacBook-Air.local>
2013-09-05 20:20:12 -0400

    Sohil Habib | First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnDota::Application.config.secret_key_base = '2b887e701bacd5807bb1392a84985c2660f0c6d5150f40c2ad5afe75f69e5e00f83690573da69f24be3e4c40e98035e4326ccee0edc5448ecf42830ad13f215a'
