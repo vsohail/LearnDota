9739b163ed925b6044fea6d97622675cb1618cee
Sohil Habib <vsohail09@gmail.com>
2013-09-07 01:32:25 -0400

    Sohil Habib | Views for deliverable 1 completed partially

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ LearnDota::Application.routes.draw do
   resources :skills
 
   resources :heros
-
+  root 'heros#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
