e5feeedcaad76c5fbe5c8a973910c0e1d9d4eafb
Sohil Habib <vsohail09@gmail.com>
2013-09-06 23:31:37 -0400

    Sohil Habib | DB somewhat final,Work on views

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,14 @@
 class Hero < ActiveRecord::Base
   has_many :guides
   has_many :skills
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates :oftype, inclusion: { in: %w(Strength Agility Intelligence),
+                                message: "should be Strength,Agility,Intelligence" }
+  validates_presence_of :faction
+  validates :faction, inclusion: { in: %w(Radiant Dire),
+                                  message: "should be Radiant,Dire" }
+  validates_presence_of :melrange
+  validates :melrange, inclusion: { in: %w(Melee Range),
+                                  message: "should be Melee,Range" }
 end
